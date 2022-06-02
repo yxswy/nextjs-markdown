aedb2064caeb442e093e8f4bf232bfc2d3a55692
yang.dongping <18267094443@163.com>
2022-06-02 10:57:54 +0800

    测试 seo

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,21 +19,17 @@ export default function Layout({
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="检索一下"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+          <meta name="description" content="淘宝网 - 亚洲较大的网上交易平台，提供各类服饰、美容、家居、数码、话费/点卡充值… 数亿优质商品，同时提供担保交易(先收货后付款)等安全交易保障服务，并由商家提供退货承诺、破损补寄等消费者保障服务，让你安心享受网上购物乐趣！" />
+          <meta name="aplus-xplug" content="NONE" />
+          <meta name="keyword" content="淘宝,掏宝,网上购物,C2C,在线交易,交易市场,网上交易,交易市场,网上买,网上卖,购物网站,团购,网上贸易,安全购物,电子商务,放心买,供应,买卖信息,网店,一口价,拍卖,网上开店,网络购物,打折,免费开店,网购,频道,店铺" />
+          <meta name="og:title" property="og:title" content="Your Awesome Open Graph Title" />
           <title>烟熏三文鱼的网站Title</title>
       </Head>
       <header className={styles.header}>
+          <img src="https://gw.alicdn.com/tfs/TB1DP0yAoY1gK0jSZFMXXaWcVXa-1190-70.png" alt="淘小铺" style={{
+              width:'1190px',
+              height:'70px'
+          }} />
         {home ? (
           <>
             {/* <Image
@@ -68,8 +64,8 @@ export default function Layout({
           </>
         )}
       </header>
-        <h1 style={{ 'display': 'none' }}>烟熏三文鱼的网站</h1>
-        <title>烟熏三文鱼的网站Title</title>
+        <h1>大标题</h1>
+        <h2>大标题</h2>
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
